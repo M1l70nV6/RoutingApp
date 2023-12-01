4dfd8c1e9e2ca34c2cdd342ba003c34c43ed5885
M1l70NV6 <miltonvg@uets.edu.ec>
2023-12-01 09:25:43 -0500

    Uso de loadChildren✅
    Define las rutas en el archivo de routing de cada tab.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,29 +7,29 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+      {//Instanciar la pagina hija Inicio
+        path:"inicio",
+        loadChildren: ()=>import("./inicio/inicio.module").then(hijo=>hijo.InicioPageModule)
       },
-      {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      {//Instanciar la pagina hija nudo
+        path:"nudo",
+        loadChildren: ()=>import("./nudo/nudo.module").then(hijo=>hijo.NudoPageModule)
       },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      {//Instanciar la pagina hija desenlace
+        path:"desenlace",
+        loadChildren: ()=>import("./desenlace/desenlace.module").then(hijo=>hijo.DesenlacePageModule)
       },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
+      {//Por defecto abrir inicio
+        path:"",
+        redirectTo:"inicio",
+        pathMatch:"full"
       }
     ]
   },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
+  {//padre por defecto
+    path:"",
+    redirectTo:"tabs/inicio",
+    pathMatch:"full"
   }
 ];
 
